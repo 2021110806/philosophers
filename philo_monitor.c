e2502c4755808e5a45304a79dc7bea4abc3cdf8b
2021110806 <2021110806@dgu.ac.kr>
2023-08-18 20:34:06 +0900

    chore: died_philo flag 위치 변경

--- philo_monitor.c
+++ philo_monitor.c
@@ -6,7 +6,7 @@
 /*   By: minjeon2 <qwer10897@naver.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 20:19:14 by minjeon2          #+#    #+#             */
-/*   Updated: 2023/08/18 20:12:21 by minjeon2         ###   ########.fr       */
+/*   Updated: 2023/08/18 20:30:05 by minjeon2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,8 @@ t_philo_info *philo_info, pthread_mutex_t *printf_mutex)
 	pthread_mutex_lock((philo_info -> eating_mutex));
 	if (curr_time - philo -> last_eating >= philo_info -> time_to_die)
 	{
-		die(philo, printf_mutex);
 		philo_info -> died_philo = 1;
+		die(philo, printf_mutex);
 		pthread_mutex_unlock((philo_info -> eating_mutex));
 		return (1);
 	}
